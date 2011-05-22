8cb57f5efe65321ab8f3e6d3db1fbaed74cc602e
politechnik66@gmail.com <politechnik66@gmail.com@6f98ddbd-1903-1a28-0232-f11f1b04f47c>
2011-05-22 09:04:36 +0000

    Начальная загрузка

---
+++ FeedbackService.Tests/HomeControllerTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using FeedbackService.Controllers;
+
+namespace FeedbackService.Tests
+{
+    [TestFixture]
+    public class HomeControllerIndexTests
+    {
+        [Test]
+        public void Puts_Message_In_ViewBag()
+        {
+            var controller = new HomeController();
+            var result = controller.Index();
+            Assert.IsNotNull(result.ViewBag.Message);
+        }
+    }
+}
